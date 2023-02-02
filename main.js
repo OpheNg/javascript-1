015926f4e56744fa5b9d5d1b8420fdaaa39acccb
Cevdet <111880570+cevdet25@users.noreply.github.com>
2023-02-02 16:39:15 +0300

    created playRound fundtion

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let playerSelection= prompt('What will you chose? :\n - rock\n - paper\n -scissors');
+let playerSelection= prompt('What will you chose? :\n - rock\n - paper\n -scissors').toLowerCase();
 let playerName= prompt("What's your name ?");
 
 function computerPlay(){
@@ -7,6 +7,23 @@ function computerPlay(){
     return computerChoice;
 }
 
-function playRound(playerSelection, computerSelection){
+const computerSelection = computerPlay();
 
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return `${playerSelection} cannot beat ${computerSelection} because they are same`;
+    }else if(playerSelection == "rock" && computerSelection == "scissors"){
+        return "You Win! Rock beats Scissors";
+    }else if(playerSelection == "rock" && computerSelection == "paper"){
+        return "You Lose! Paper beats Rock";
+    }else if(playerSelection == "paper" && computerSelection == "rock"){
+        return "You Win! Paper beats Rock";
+    }else if(playerSelection == "paper" && computerSelection == "scissors"){
+        return "You Lose! Scissors beats Paper";
+    }else if(playerSelection == "scissors" && computerSelection == "rock"){
+        return "You Lose! Rock beats Scissors";
+    }else if(playerSelection == "scissors" && computerSelection == "paper"){
+        return "You Win! sSissors beats Paper";
+    }
 }
+
